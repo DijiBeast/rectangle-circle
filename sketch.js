53cc9bc34d4760918bd6d255cea02f46df985121
DijiBeast <86576666+DijiBeast@users.noreply.github.com>
2022-03-06 17:09:36 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill("#595353");
+  stroke("#595353");
+  circle(40, 40, 50);
+  rect(25, 50, 30, 300);
+  circle(40, 350, 50);
+  rect(25, 335, 300, 30);
+   circle(335, 350, 50);
+    rect(325, 60, 30, 300);
+   circle(340, 50, 50);
+  rect(30, 30, 300, 30)
+}
